db85f045e13943bba3302ccaed2edd445587a97b
Saba Izraqi <izraqisaba@gmail.com>
2025-05-05 19:57:18 +0300

    feat (services): implement officer service with create function only.

--- src/domain/entities/officer.entity.ts
+++ src/domain/entities/officer.entity.ts
@@ -9,7 +9,7 @@ import {
 import { BaseEntity } from "./base.entity";
 import { Prison } from "./prison.entity";
 import { Cell } from "./cell.entity";
-import { Shift, OfficerRole } from "../enum";
+import { Shift, OfficerRole } from "../enums";
 
 @Entity('officers')
 export class Officer extends BaseEntity {
